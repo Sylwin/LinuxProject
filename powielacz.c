191819dc67d1afb8d5ac589e06b9b6c38183ad27
Sylwia <sylenit@gmail.com>
2017-01-03 09:36:59 +0100

    Main program with getopt

---
+++ powielacz.c
@@ -0,0 +1,48 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <signal.h>
+#include <time.h>
+#include <math.h>
+#include <errno.h>
+#include <string.h>
+
+char filesNamePattern[20];
+char diagnosticPath[20];
+int numberOfFifos;
+
+int main(int argc, char* argv[])
+{
+    int opt;
+
+    while ((opt = getopt(argc, argv, "p:c:L:")) != -1)
+    {
+        switch (opt)
+        {
+        case 'p':
+            strcpy(filesNamePattern, optarg);
+            break;
+        case 'c':
+            numberOfFifos = atoi(optarg);
+            break;
+        case 'L':
+            strcpy(diagnosticPath,optarg);
+            break;
+        case ':':
+            fprintf(stderr, "%s: option '-%c' requires an argument\n", argv[0], optopt);
+            break;
+        case '?':
+        default:
+            fprintf(stderr, "%s: option '-%c' is invalid: ignored\n", argv[0], optopt);
+            break;
+        }
+    }
+
+    if(argc < 2)
+    {
+        printf("Usage : %s -p string -c int [-L string]\n", argv[0]);
+        return 0;
+    }
+
+    return 0;
+}
